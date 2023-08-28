9e0b96b331a69e81375d432f5650b980cb796bb7
j-ackyao <wfy1001@gmail.com>
2023-08-28 14:49:37 -0700

    full working z test graphs

--- functions/statfuncs.js
+++ functions/statfuncs.js
@@ -15,6 +15,16 @@ function normcdf(x, width = APPROX_WIDTH, stop_sd = STOP_SD) {
     return sum * width;
 } 
 
+function normq(x, width = APPROX_WIDTH, stop_sd = STOP_SD) {
+    curr_x = -STOP_SD;
+    var sum = normpdf(-stop_sd * 2) / 2;
+    while (sum * width < x) {
+        curr_x += width;
+        sum += normpdf(curr_x);
+    }
+    return curr_x;
+}
+
 function normpdf(x, mu = 0, sd = 1) {
     return 1 / (sd * Math.sqrt(2 * Math.PI) * Math.exp(0.5 * Math.pow((x-mu)/sd, 2)));
 }
@@ -44,8 +54,12 @@ const NULL_COLOR = "#555555";
 const ALT_COLOR = "#000000";
 const P_COLOR = "#FF0000";
 const P_ALPHA = 0.3;
+const I_COLOR = "#00FF00";
+const II_COLOR = "#0000FF";
+const ERROR_ALPHA = 0.2;
+const SL_COLOR = "#FF0000";
 
-function drawmodels(canvas, test, alt_hyp) {
+function drawmodels(canvas, test, alt_hyp, shade, sl) {
 
     var displayWidth = canvas.clientWidth;
     var displayHeight = canvas.clientHeight;
@@ -57,23 +71,51 @@ function drawmodels(canvas, test, alt_hyp) {
     context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
 
+    if (shade == "p") {
+        // shade p value
+        var dir = (alt_hyp == "lt") ? "left" : ((alt_hyp == "gt") ? "right" : "both");
+        fillmodel(canvas, 0, 1, test, dir, P_COLOR, P_ALPHA);
+    } else {
+        // shade errors
+        var dir = (alt_hyp == "lt") ? "left" : ((alt_hyp == "gt") ? "right" : "both");
+        if (dir == "both") {
+            fillmodel(canvas, 0, 1, (1 - 2 * (test > 0)) * normq(sl / 2), dir, I_COLOR, ERROR_ALPHA);
+            fillmodel(canvas, test / 2, 1, normq(sl / 2), dir, II_COLOR, ERROR_ALPHA, -normq(sl / 2))
+        }
+        else {
+            fillmodel(canvas, 0, 1, (-2 * (dir == "right") + 1) * normq(Number(sl)), dir, I_COLOR, ERROR_ALPHA);
+            fillmodel(canvas, test / 2, 1, (-2 * (dir == "right") + 1) * normq(Number(sl)), (dir == "left") ? "right" : "left", II_COLOR, ERROR_ALPHA);
+        }
+    }
 
-    // shade p value
-    var dir = (alt_hyp == "lt") ? "left" : ((alt_hyp == "gt") ? "right" : "both");
-    fillmodel(canvas, 0, 1, test, dir, P_COLOR, P_ALPHA);
+    // draw significance level and axis
+    context.lineWidth = LINE_WIDTH / 2;
+    context.beginPath();
+    context.strokeStyle = SL_COLOR;
+    if (alt_hyp == "ne") {
+        context.moveTo(canvas.width / 2 + normq(Number(sl)/2) * H_SCALE * canvas.width / 2, canvas.height - normpdf(normq(Number(sl)/2)) * V_SCALE * canvas.height - CANVAS_OFFSET);
+        context.lineTo(canvas.width / 2 + normq(Number(sl) / 2) * H_SCALE * canvas.width / 2, canvas.height - CANVAS_OFFSET);
+        context.moveTo(canvas.width / 2 - normq(Number(sl) / 2) * H_SCALE * canvas.width / 2, canvas.height - normpdf(normq(Number(sl) / 2)) * V_SCALE * canvas.height - CANVAS_OFFSET);
+        context.lineTo(canvas.width / 2 - normq(Number(sl) / 2) * H_SCALE * canvas.width / 2, canvas.height - CANVAS_OFFSET);
+    }
+    else {
+        context.moveTo(canvas.width / 2 + (1 + -2 * (alt_hyp == "gt")) * normq(Number(sl)) * H_SCALE * canvas.width / 2, canvas.height - normpdf(normq(Number(sl))) * V_SCALE * canvas.height - CANVAS_OFFSET);
+        context.lineTo(canvas.width / 2 + (1 + -2 * (alt_hyp == "gt")) * normq(Number(sl)) * H_SCALE * canvas.width / 2, canvas.height - CANVAS_OFFSET);
+    }
+    context.stroke();
+
+    context.strokeStyle = "#000000";
+    context.beginPath();
+    context.moveTo(0, canvas.height - CANVAS_OFFSET);
+    context.lineTo(canvas.width, canvas.height - CANVAS_OFFSET);
+    context.stroke();
+
+
 
     // draw distributions
     drawmodel(canvas, 0, 1, NULL_COLOR);
     drawmodel(canvas, test, 1, ALT_COLOR);
 
-    // draw axis
-    context.lineWidth = LINE_WIDTH/2;
-    context.beginPath();
-    context.moveTo(0, canvas.height - CANVAS_OFFSET);
-    context.lineTo(canvas.width, canvas.height - CANVAS_OFFSET);
-
-    context.stroke();
-    context.closePath();
 }
 
 function drawmodel(canvas, mean, sd, color) {
@@ -101,16 +143,23 @@ function drawmodel(canvas, mean, sd, color) {
 
 }
 
-function fillmodel(canvas, mean, sd, from, dir, color, alpha) {
+function fillmodel(canvas, mean, sd, from, dir, color, alpha, to = null) {
     const context = canvas.getContext("2d");
     const density = normd(mean, sd);
     const width = canvas.width;
     const height = canvas.height;
 
-
     context.moveTo(0, height - density[1][0] - CANVAS_OFFSET);
 
-    if (dir == "left") {
+    if (to != null) {
+        context.beginPath();
+        for (var i = 0; i < density[0].length; i++) {
+            if (from < density[0][i] && density[0][i] < to) {
+                context.lineTo(width / 2 + density[0][i] * H_SCALE * width / 2, height - density[1][i] * V_SCALE * height - CANVAS_OFFSET);
+            }
+        }
+        context.lineTo(width / 2 + to * H_SCALE * width / 2, height - CANVAS_OFFSET);
+    } else if (dir == "left") {
         context.beginPath();
         for (var i = 0; i < density[0].length; i++) {
             context.lineTo(width / 2 + density[0][i] * H_SCALE * width / 2, height - density[1][i] * V_SCALE * height - CANVAS_OFFSET);
@@ -126,23 +175,22 @@ function fillmodel(canvas, mean, sd, from, dir, color, alpha) {
                 break;
             }
         }
-    } else {
+    } else if (dir == "both") {
         if (from < mean) {
-            fillmodel(canvas, mean, sd, from, "left", color);
-            fillmodel(canvas, mean, sd, mean + (mean - from), "right", color);
+            fillmodel(canvas, mean, sd, mean + (mean - from), "right", color, alpha);
+            fillmodel(canvas, mean, sd, from, "left", color, alpha);
         } else {
-            fillmodel(canvas, mean, sd, from, "right", color);
-            fillmodel(canvas, mean, sd, mean + (mean - from), "left", color);
+            fillmodel(canvas, mean, sd, mean + (mean - from), "left", color, alpha);
+            fillmodel(canvas, mean, sd, from, "right", color, alpha);
         }
     }
 
     context.lineTo(width / 2 + from * H_SCALE * width / 2, height - CANVAS_OFFSET);
 
-    context.globalAlpha = P_ALPHA;
+    context.globalAlpha = alpha;
     context.fillStyle = color;
     context.fill();
     context.globalAlpha = 1;
-    context.closePath();
 
 }
 
--- functions/ztest.js
+++ functions/ztest.js
@@ -7,6 +7,8 @@ function ztestmean() {
     const mean = document.getElementById("mean_mean").value;
     const size = document.getElementById("size_mean").value;
     const alt_hyp = document.getElementById("ah_mean").value;
+    const sl = document.getElementById("sl_mean").value;
+    const shade = document.getElementById("shade_mean").value;
 
 
     var teststat = "";
@@ -49,7 +51,14 @@ function ztestmean() {
             error = true;
         }
     }
-
+    if (sl == "") {
+        document.getElementById("warning_mean").innerHTML += "significance level is empty <br/>";
+        error = true;
+    }
+    else if (sl < 0 || sl > 1) {
+        document.getElementById("warning_mean").innerHTML += "significance level is invalid <br/>";
+        error = true;
+    }       
     if (error) {
         return;
     }
@@ -72,12 +81,40 @@ function ztestmean() {
     }
     pvalue = Math.round(pvalue * Math.pow(10, ROUND_DIGITS)) / Math.pow(10, ROUND_DIGITS);
 
+    if (pvalue < sl) {
+        document.getElementById("conclusion_mean").innerHTML = "reject the null hypothesis";
+    } else {
+        document.getElementById("conclusion_mean").innerHTML = "failed to reject the null hypothesis";
+    }
 
     document.getElementById("teststat_mean").innerHTML = teststat;
     document.getElementById("pvalue_mean").innerHTML = pvalue;
+    document.getElementById("typei_mean").innerHTML = sl;
+
+    typeii = "";
+
+    if (alt_hyp == "ne") {
+        if (teststat > 0) {
+            typeii = normcdf(((-normq(Number(sl) / 2) * Number(sd) / Math.sqrt(size)) + Number(hyp) - Number(mean)) / (Number(sd) / Math.sqrt(size)));
+            typeii -= normcdf(((normq(Number(sl) / 2) * Number(sd) / Math.sqrt(size)) + Number(hyp) - Number(mean)) / (Number(sd) / Math.sqrt(size)));
+        }
+        else {
+            typeii = 1 - normcdf(((normq(Number(sl) / 2) * Number(sd) / Math.sqrt(size)) + Number(hyp) - Number(mean)) / (Number(sd) / Math.sqrt(size)));
+            typeii -= 1 - normcdf(((-normq(Number(sl) / 2) * Number(sd) / Math.sqrt(size)) + Number(hyp) - Number(mean)) / (Number(sd) / Math.sqrt(size)));
+        }
+    }
+    else if (alt_hyp == "lt") {
+        typeii = 1 - normcdf(((normq(Number(sl)) * Number(sd) / Math.sqrt(size)) + Number(hyp) - Number(mean)) / (Number(sd) / Math.sqrt(size)));
+    }
+    else if (alt_hyp == "gt") {
+        typeii = normcdf(((-normq(Number(sl)) * Number(sd) / Math.sqrt(size)) + Number(hyp) - Number(mean)) / (Number(sd) / Math.sqrt(size)));
+    }
+    typeii = Math.round(typeii * Math.pow(10, ROUND_DIGITS)) / Math.pow(10, ROUND_DIGITS);
+
+    document.getElementById("typeii_mean").innerHTML = typeii;
 
     var canvas = document.getElementById("mean_model");
-    drawmodels(canvas, teststat, alt_hyp);
+    drawmodels(canvas, teststat, alt_hyp, shade, sl);
 }
 
 
@@ -86,6 +123,8 @@ function ztestprop() {
     const mean = document.getElementById("mean_prop").value;
     const size = document.getElementById("size_prop").value;
     const alt_hyp = document.getElementById("ah_prop").value;
+    const sl = document.getElementById("sl_prop").value;
+    const shade = document.getElementById("shade_prop").value;
 
 
     var teststat = "";
@@ -128,33 +167,72 @@ function ztestprop() {
             error = true;
         }
     }
+    if (sl == "") {
+        document.getElementById("warning_prop").innerHTML += "significance level is empty <br/>";
+        error = true;
+    }
+    else if (sl < 0 || sl > 1) {
+        document.getElementById("warning_prop").innerHTML += "significance level is invalid <br/>";
+        error = true;
+    }    
+
+
+    if (error) {
+        return;
+    }
 
 
     var sd = Math.sqrt(hyp * (1 - hyp) / size)
-    if (!error) {
-        teststat = (mean - hyp) / sd;
+    teststat = (mean - hyp) / sd;
 
-        if (alt_hyp == "ne") {
-            if (teststat > 0) {
-                pvalue = (1 - normcdf(teststat)) * 2;
-            }
-            else {
-                pvalue = (normcdf(teststat)) * 2;
-            }
+    if (alt_hyp == "ne") {
+        if (teststat > 0) {
+            pvalue = (1 - normcdf(teststat)) * 2;
         }
-        else if (alt_hyp == "lt") {
-            pvalue = normcdf(teststat);
+        else {
+            pvalue = (normcdf(teststat)) * 2;
         }
-        else if (alt_hyp == "gt") {
-            pvalue = 1-normcdf(teststat);
-        }
-        pvalue = Math.round(pvalue * Math.pow(10, ROUND_DIGITS)) / Math.pow(10, ROUND_DIGITS);
+    }
+    else if (alt_hyp == "lt") {
+        pvalue = normcdf(teststat);
+    }
+    else if (alt_hyp == "gt") {
+        pvalue = 1 - normcdf(teststat);
+    }
+    pvalue = Math.round(pvalue * Math.pow(10, ROUND_DIGITS)) / Math.pow(10, ROUND_DIGITS);
+
+    if (pvalue < sl) {
+        document.getElementById("conclusion_prop").innerHTML = "reject the null hypothesis";
+    } else {
+        document.getElementById("conclusion_prop").innerHTML = "failed to reject the null hypothesis";
     }
 
     document.getElementById("teststat_prop").innerHTML = teststat;
     document.getElementById("pvalue_prop").innerHTML = pvalue;
+    document.getElementById("typei_prop").innerHTML = sl;
+
+    typeii = "";
+
+    if (alt_hyp == "ne") {
+        if (teststat > 0) {
+            typeii = normcdf(((-normq(Number(sl) / 2) * Number(sd)) + Number(hyp) - Number(mean)) / (Number(sd)));
+            typeii -= normcdf(((normq(Number(sl) / 2) * Number(sd)) + Number(hyp) - Number(mean)) / (Number(sd)));
+        }
+        else {
+            typeii = 1 - normcdf(((normq(Number(sl) / 2) * Number(sd)) + Number(hyp) - Number(mean)) / (Number(sd)));
+            typeii -= 1 - normcdf(((-normq(Number(sl) / 2) * Number(sd)) + Number(hyp) - Number(mean)) / (Number(sd)));
+        }
+    }
+    else if (alt_hyp == "lt") {
+        typeii = 1 - normcdf(((normq(Number(sl)) * Number(sd)) + Number(hyp) - Number(mean)) / (Number(sd)));
+    }
+    else if (alt_hyp == "gt") {
+        typeii = normcdf(((-normq(Number(sl)) * Number(sd)) + Number(hyp) - Number(mean)) / (Number(sd)));
+    }
+    typeii = Math.round(typeii * Math.pow(10, ROUND_DIGITS)) / Math.pow(10, ROUND_DIGITS);
+    document.getElementById("typeii_prop").innerHTML = typeii;
 
 
     var canvas = document.getElementById("prop_model");
-    drawmodels(canvas, teststat, alt_hyp);
+    drawmodels(canvas, teststat, alt_hyp, shade, sl);
 }
